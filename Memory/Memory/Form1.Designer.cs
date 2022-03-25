c66e8576d6de91c26bd268eb80855c1659f36d5a
Daniel Henel <danielhenel@student.agh.edu.pl>
2022-03-25 10:56:57 +0100

    initial

---
+++ Memory/Memory/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.easyButton = new System.Windows.Forms.Button();
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // easyButton
+            // 
+            this.easyButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.easyButton.Font = new System.Drawing.Font("MV Boli", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.Location = new System.Drawing.Point(55, 189);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(344, 91);
+            this.easyButton.TabIndex = 0;
+            this.easyButton.Text = "easy (48)";
+            this.easyButton.UseVisualStyleBackColor = false;
+            this.easyButton.Click += new System.EventHandler(this.easyButtonClick);
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.mediumButton.Font = new System.Drawing.Font("MV Boli", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumButton.Location = new System.Drawing.Point(55, 302);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(344, 91);
+            this.mediumButton.TabIndex = 1;
+            this.mediumButton.Text = "medium (80)";
+            this.mediumButton.UseVisualStyleBackColor = false;
+            this.mediumButton.Click += new System.EventHandler(this.mediumButtonClick);
+            // 
+            // hardButton
+            // 
+            this.hardButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.hardButton.Font = new System.Drawing.Font("MV Boli", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.Location = new System.Drawing.Point(55, 412);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(344, 91);
+            this.hardButton.TabIndex = 2;
+            this.hardButton.Text = "hard (120)";
+            this.hardButton.UseVisualStyleBackColor = false;
+            this.hardButton.Click += new System.EventHandler(this.hardButtonClick);
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("MV Boli", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(99, 76);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(275, 49);
+            this.title.TabIndex = 3;
+            this.title.Text = "memory game";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(457, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Controls.Add(this.easyButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Memory/Memory/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void easyButtonClick(object sender, EventArgs e)
+        {
+            Form2 game = new Form2();
+            TableLayoutPanel tab = new TableLayoutPanel();
+            tab.RowCount = 6;
+            tab.ColumnCount = 8;
+            
+            game.ShowDialog();
+        }
+
+        private void mediumButtonClick(object sender, EventArgs e)
+        {
+            Form2 game = new Form2();
+            TableLayoutPanel tab = new TableLayoutPanel();
+            tab.RowCount = 8;
+            tab.ColumnCount = 10;
+
+            game.ShowDialog();
+        }
+
+        private void hardButtonClick(object sender, EventArgs e)
+        {
+            Form2 game = new Form2();
+            TableLayoutPanel tab = new TableLayoutPanel();
+            tab.RowCount = 10;
+            tab.ColumnCount = 12;
+            tab.
+            game.ShowDialog();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
