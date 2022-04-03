cf7e3e97f7acdac250d6395ab051598f1a8b9ad8
Daniel Henel <danielhenel@student.agh.edu.pl>
2022-04-03 21:29:32 +0200

    Revert "update style class"
    This reverts commit b5668870ddccd28481a940b65ccbd5a28165abfa.

--- Memory/Memory/Style.cs
+++ Memory/Memory/Style.cs
@@ -11,12 +11,11 @@ namespace Memory
         static System.Drawing.Color backgroundColor = System.Drawing.SystemColors.Info;
         static System.Drawing.Color buttonColor = System.Drawing.SystemColors.MenuHighlight;
         static System.Drawing.Font buttonFont = new System.Drawing.Font("MV Boli", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-        static System.Drawing.Font labelFont = new System.Drawing.Font("MV Boli", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 
         public static System.Drawing.Color getBackgroundColor()
         {
             return backgroundColor;
-        } 
+        }
 
         public static System.Drawing.Color getButtonColor()
         {
