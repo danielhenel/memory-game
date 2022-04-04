2b438d68dfa62b63073e9377c6ffd38b7fb0cc08
Daniel Henel <danielhenel@student.agh.edu.pl>
2022-04-04 13:42:21 +0200

    deleted images

--- Memory/Memory/Properties/Resources.Designer.cs
+++ Memory/Memory/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace Memory.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap settings {
-            get {
-                object obj = ResourceManager.GetObject("settings", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
